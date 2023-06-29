71fa03bda7ac14d6bf588a4c29f9d52fdc851922
Kenroy George <kenroy@garakami.com>
2023-06-29 21:38:56 +0100

    fix: update button type on tickets

--- components/layout/tickets.tsx
+++ components/layout/tickets.tsx
@@ -279,7 +279,8 @@ export default function Tickets({
 
         <div className="flex flex-col justify-stretch px-4 py-4 sm:px-4">
           <button
-            type="button"
+            // type="button"
+            className="inline-flex items-center justify-center rounded-md border border-transparent bg-purple-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2"
             disabled={!buttonStatus(session, step, isFilled)![1]}
             onClick={(e) => {
               e.preventDefault();
@@ -289,7 +290,6 @@ export default function Tickets({
                 setStep(2);
               }
             }}
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-purple-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
           >
             {buttonStatus(session, step, isFilled)![0].toString()}
           </button>
